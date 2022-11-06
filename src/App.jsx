19020936a5c1b6e6d8aaeab96d0639dffde60847
gidex4real <simongideongidex@gmail.com>
2022-11-06 22:46:44 +0100

    removed lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { lazy, Suspense } from 'react';
 import { Route, Routes } from "react-router-dom";
 import './App.css'
-import { Home, NewRepo, ErrorFallback, ErrorPage } from './component';
+import { Home, Repo, NewRepo, ErrorFallback, ErrorPage } from './component';
 import {ErrorBoundary} from 'react-error-boundary';
-
-const Repo = lazy(() => import('./component/Repo'));
 
 function App() {
   return (
     <div className="App">
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Suspense fallback={<h1>Still Loading Profile Image...</h1>}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path="repo">
-              <Route index element={<Repo />}/>
-              <Route path=":id" element={<NewRepo />}/>
-            </Route>
-            <Route path='error' element={<ErrorFallback/>} />
-            <Route path='*' element={<ErrorPage />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="repo">
+            <Route index element={<Repo />}/>
+            <Route path=":id" element={<NewRepo />}/>
+          </Route>
+          <Route path='error' element={<ErrorFallback/>} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
       </ErrorBoundary>
     </div>
   )
